7b1ff25bcd296188e0ff6811a33f56dd999e12bb
TranThuAn99w <17130003@st.hcmuaf.edu.vn>
2021-06-06 05:13:12 +0700

    responsive-update

--- js/style_animation.js
+++ js/style_animation.js
@@ -64,15 +64,6 @@ $(document).ready(function(){
     if(count < procedures.length){
         showProcedure(count, lastProc);
         showIndicator(count, lastIndic);
-    // $("#procedure__step-"+count).removeClass("hide");
-    // $("#procedure__step-"+count).addClass("show");
-    // $("#indicator-"+count).removeClass("unactive");
-    // $("#indicator-"+count).addClass("active");
-
-    // $("#procedure__step-"+lastProc).addClass("hide");
-    // $("#procedure__step-"+lastProc).removeClass("show");
-    // $("#indicator-"+lastIndic).addClass("unactive");
-    // $("#indicator-"+lastIndic).removeClass("active");
     lastProc = count;
     lastIndic = count;
     }
@@ -90,18 +81,50 @@ $(document).ready(function(){
     if(count>=0){
         showProcedure(count, lastProc);
         showIndicator(count, lastIndic);
-    // $("#procedure__step-"+count).removeClass("hide");
-    // $("#procedure__step-"+count).addClass("show");
-    // $("#indicator-"+count).removeClass("unactive");
-    // $("#indicator-"+count).addClass("active");
-
-    // $("#procedure__step-"+lastProc).addClass("hide");
-    // $("#procedure__step-"+lastProc).removeClass("show");
-    // $("#indicator-"+lastIndic).addClass("unactive");
-    // $("#indicator-"+lastIndic).removeClass("active");
     lastProc = count;
     lastIndic = count;
     }
+    });
+
+    $("#button_right-mb").click(function(e){
+        e.preventDefault();
+        console.log("last-procedure--"+lastProc);
+    console.log("last-indicator--"+lastIndic);
+    console.log("current-Index--" +count);
+    console.log("=========================");
+
+        var count = lastProc + 1;
+        if(count < procedures.length){
+            $("#procedure__step-"+count).addClass("show");
+            $("#procedure__step-"+count).removeClass("hide");
+
+            $("#procedure__step-"+lastProc).removeClass("show");
+            $("#procedure__step-"+lastProc).addClass("hide");
+            lastProc = count;
+            var htmlIndicator = $("#indicator-"+count).html();
+            $("#get-name-step").html(htmlIndicator);
+        }
+    });
+    $("#button_left-mb").click(function(e){
+        e.preventDefault();
+        console.log("last-procedure--"+lastProc);
+    console.log("last-indicator--"+lastIndic);
+    console.log("current-Index--" +count);
+    console.log("=========================");
+
+        var count = lastProc - 1;
+        if(count >= 0){
+            $("#procedure__step-"+count).addClass("show");
+            $("#procedure__step-"+count).removeClass("hide");
+
+            $("#procedure__step-"+lastProc).removeClass("show");
+            $("#procedure__step-"+lastProc).addClass("hide");
+            lastProc = count;
+            var htmlIndicator = $("#indicator-"+count).html();
+            console.log(htmlIndicator);
+            $("#get-name-step").html(htmlIndicator);
+           
+        }
     });
 
     function showIndicator(x, currentIndex){
